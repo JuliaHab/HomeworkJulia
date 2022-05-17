6c8a980d199b90de34b865b048c58ca284f61972
JuliaHab <104641022+JuliaHab@users.noreply.github.com>
2022-05-17 12:30:38 +0300

    Homework - Julia

--- src/test/java/selenium3and4/Koel.java
+++ src/test/java/selenium3and4/Koel.java
@@ -1,4 +1,4 @@
-package selenium3;
+package selenium3and4;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -6,9 +6,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.util.List;
-import java.util.Set;
 
 // Login
 public class Koel {
@@ -43,6 +40,34 @@ public class Koel {
         Assert.assertTrue(homeIcon.isDisplayed());
 
         // прописали через какое время закрыть браузер (указано 5 секунд)
+        Thread.sleep(5000);
+        driver.quit();
+    }
+
+    @Test
+    public void loginToKoel_incorrectLogin() throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://bbb.testpro.io");
+
+        By emailFieldLocator = By.xpath("//*[@type='email']");
+        WebElement emailField = driver.findElement(emailFieldLocator);
+        emailField.sendKeys("Flower_cool@mail.ru");
+
+        By passwordFieldLocator = By.cssSelector("[type=\"password\"]");
+        WebElement passwordField = driver.findElement(passwordFieldLocator);
+        passwordField.sendKeys("errorPassword");
+
+        By loginButtonLocator = By.xpath("//*[@type=\"submit\"]");
+        WebElement loginButton = driver.findElement(loginButtonLocator);
+        loginButton.click();
+        Thread.sleep(300);
+
+        By errorFrameLocator = By.className("error");
+        WebElement errorFrame = driver.findElement(errorFrameLocator);
+        Assert.assertTrue(errorFrame.isDisplayed());
+
         Thread.sleep(5000);
         driver.quit();
     }
